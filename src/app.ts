db2fc2da03c16304dec9275138dae0c80b1a1e42
rickywid <rickywid@hotmail.com>
2020-06-12 23:06:30 -0400

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import path from 'path';
 const app = express()
+const port = 5000
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.get('/api/users', (req, res) => res.json({users: ['bob']}))
 
-app.listen(process.env.PORT || 5000, () => console.log(`Example app listening at http://localhost:${process.env.PORT}`))
+app.listen(process.env.PORT || 5000, () => console.log(`Example app listening at http://localhost:${port}`))
