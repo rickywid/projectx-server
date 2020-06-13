e6e093cf1114f2d2c8eda8e0f5a6f661855cd184
rickywid <rickywid@hotmail.com>
2020-06-12 22:35:14 -0400

    use port env var

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 import path from 'path';
 const app = express()
-const port = 5000
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.get('/api/users', (req, res) => res.json({users: ['bob']}))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(process.env.PORT || 5000, () => console.log(`Example app listening at http://localhost:${port}`))
