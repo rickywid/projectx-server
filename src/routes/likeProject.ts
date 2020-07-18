22f45ce440cf6a645ea8a10f3fbbd08a8a95fb85
rickywid <rickywid@hotmail.com>
2020-07-18 00:19:56 -0400

    check if user authenticated

--- src/routes/likeProject.ts
+++ src/routes/likeProject.ts
@@ -5,39 +5,45 @@ const router = express.Router();
 
 router.post('/:id', function (req, res, next) {
 
-    const id = req.params.id;
-    const form = new formidable.IncomingForm();
+   /**
+    * Protected Endpoint - User must be authenticated
+    */
 
-    form.parse(req, async (err, fields, files) => {
-        if (err) throw err;
-        // add new like and get latest like count and list of user id's
-
-        db.query(`
-        INSERT INTO likes (user_id, project_id)
-        VALUES ($1, $2);
-    `, [fields.user_id, id], (err: any, result: { rows: any; }) => {
-            if (err) { console.log(err) };
-
+    if(req?.isAuthenticated()) {
+        const id = req.params.id;
+        const form = new formidable.IncomingForm();
+    
+        form.parse(req, async (err, fields, files) => {
+            if (err) throw err;
+    
             db.query(`
-        SELECT
-            (SELECT array_agg(user_id) AS users 
-            FROM likes 
-            WHERE project_id = $1),
-            COUNT(*)
-        FROM likes
-        WHERE project_id = $1;
-        `, [id], (err: any, result: { rows: any; }) => {
+            INSERT INTO likes (user_id, project_id)
+            VALUES ($1, $2);
+        `, [fields.user_id, id], (err: any, result: { rows: any; }) => {
                 if (err) { console.log(err) };
-                const data = result.rows[0];
-
-                // prevent returning NULL value - return an empty array if no users has liked the project
-                data.users = data.users || []
-                res.json({
-                    data: result.rows[0]
+    
+                db.query(`
+            SELECT
+                (SELECT array_agg(user_id) AS users 
+                FROM likes 
+                WHERE project_id = $1),
+                COUNT(*)
+            FROM likes
+            WHERE project_id = $1;
+            `, [id], (err: any, result: { rows: any; }) => {
+                    if (err) { console.log(err) };
+                    const data = result.rows[0];
+    
+                    data.users = data.users || []
+                    res.json({
+                        data: result.rows[0]
+                    });
                 });
-            });
+            })
         })
-    })
+    } else {
+        res.status(401).send('Unauthorized');
+    }
 });
 
-export default router;
+export default router; 
