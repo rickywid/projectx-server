33dc14cd85f3536e0e2a9a0073629509ce4d8234
rickywid <rickywid@hotmail.com>
2020-07-19 02:44:48 -0400

    create project filter by type endpoint

--- src/routes/projectsType.ts
+++ src/routes/projectsType.ts
@@ -5,21 +5,115 @@ const router = express.Router();
 router.get('/:name', function (req, res, next) {
 
     const name = req.params.name;
-   
+    
     if(name === 'popular') {
-        db.query(``, [name], (err: any, result: { rows: any; }) => {})
+        db.query(`
+        SELECT 
+            projects.uuid,
+            projects.name,
+            projects.description,
+            projects.tagline,
+            projects.url,
+            projects.images,
+            projects.collaboration,
+            (SELECT count(*) FROM comments WHERE comments.project_id = projects.uuid) AS comment_count,
+            (SELECT count(*) FROM likes WHERE likes.project_id = projects.uuid) AS likes_count,
+            (SELECT id FROM users WHERE users.id = projects.user_id) AS user_id,
+            (SELECT username FROM users WHERE users.id = projects.user_id),
+            (SELECT gh_avatar FROM users WHERE users.id = projects.user_id),
+
+            (SELECT array_agg(technologies.name::TEXT)
+                FROM projects_technologies
+                INNER JOIN technologies
+                ON technologies.id = projects_technologies.technology_id
+                WHERE projects_technologies.project_id = projects.uuid) AS technologies,
+            
+            (SELECT array_agg(tags.name::TEXT)
+                FROM projects_tags
+                INNER JOIN tags
+                ON tags.id = projects_tags.tag_id
+                WHERE projects_tags.project_id = projects.uuid) AS tags
+        FROM projects
+        ORDER BY likes_count DESC;
+        `, [], (err: any, result: { rows: any; }) => {
+            res.json({
+                data: result.rows || []
+            });
+        });
     }
 
-    if(name === 'frontend') {
-        db.query(``, [name], (err: any, result: { rows: any; }) => {})
-    }
+    if(name === 'frontend' || name === 'fullstack') {
+        db.query(`
+        SELECT 
+            projects.uuid,
+            projects.name,
+            projects.description,
+            projects.tagline,
+            projects.url,
+            projects.images,
+            projects.collaboration,
+            (SELECT count(*) FROM comments WHERE comments.project_id = projects.uuid) AS comment_count,
+            (SELECT count(*) FROM likes WHERE likes.project_id = projects.uuid) AS likes_count,
+            (SELECT id FROM users WHERE users.id = projects.user_id) AS user_id,
+            (SELECT username FROM users WHERE users.id = projects.user_id),
+            (SELECT gh_avatar FROM users WHERE users.id = projects.user_id),
 
-    if(name === 'fullstack') {
-        db.query(``, [name], (err: any, result: { rows: any; }) => {})
+            (SELECT array_agg(technologies.name::TEXT)
+                FROM projects_technologies
+                INNER JOIN technologies
+                ON technologies.id = projects_technologies.technology_id
+                WHERE projects_technologies.project_id = projects.uuid) AS technologies,
+            
+            (SELECT array_agg(tags.name::TEXT)
+                FROM projects_tags
+                INNER JOIN tags
+                ON tags.id = projects_tags.tag_id
+                WHERE projects_tags.project_id = projects.uuid) AS tags
+        FROM projects
+        WHERE type = $1
+        ORDER BY created_on ASC;
+        `, [name], (err: any, result: { rows: any; }) => {
+            res.json({
+                data: result.rows || []
+            });
+        });
     }
 
     if(name === 'collaboration') {
-        db.query(``, [name], (err: any, result: { rows: any; }) => {})
+        db.query(`
+        SELECT 
+            projects.uuid,
+            projects.name,
+            projects.description,
+            projects.tagline,
+            projects.url,
+            projects.images,
+            projects.collaboration,
+            (SELECT count(*) FROM comments WHERE comments.project_id = projects.uuid) AS comment_count,
+            (SELECT count(*) FROM likes WHERE likes.project_id = projects.uuid) AS likes_count,
+            (SELECT id FROM users WHERE users.id = projects.user_id) AS user_id,
+            (SELECT username FROM users WHERE users.id = projects.user_id),
+            (SELECT gh_avatar FROM users WHERE users.id = projects.user_id),
+
+            (SELECT array_agg(technologies.name::TEXT)
+                FROM projects_technologies
+                INNER JOIN technologies
+                ON technologies.id = projects_technologies.technology_id
+                WHERE projects_technologies.project_id = projects.uuid) AS technologies,
+            
+            (SELECT array_agg(tags.name::TEXT)
+                FROM projects_tags
+                INNER JOIN tags
+                ON tags.id = projects_tags.tag_id
+                WHERE projects_tags.project_id = projects.uuid) AS tags
+        FROM projects
+        WHERE collaboration = true
+        ORDER BY created_on ASC;        
+        `, [], (err: any, result: { rows: any; }) => {
+            res.json({
+                data: result.rows || []
+            });
+        })
     }
 });
 
