23b994e0820598985c1dfc69d05ed3ab313f5658
rickywid <rickywid@hotmail.com>
2020-08-02 08:06:48 -0400

    update queries to LIMIT/OFFSET userProfile projects

---
+++ src/routes/getUserLikedProjects.ts
@@ -0,0 +1,89 @@
+import express from 'express';
+import db from './../lib/db';
+const router = express.Router();
+
+router.get('/:username/projects/liked', function (req, res, next) {
+    const username = req.params.username;
+    const page: any = req.query.page;
+    const offset: any = req.query.offset;
+    let loggedInUser: string;
+    
+    // check if req.user exists (if user is logged in)
+    if(req.user) {
+        const user = req.user as any;
+        loggedInUser = user.username;
+    } else {
+        // if user is accessing user profile page directly after new user signup, get username from sessions
+        loggedInUser = req!.session!.username;
+    }
+
+    // get user profile
+    db.query(`
+    SELECT 
+        users.id
+    FROM users 
+    WHERE username = $1`, [username], (err: any, result: any) => {
+        if (err) console.log(err)
+
+        if(!result.rows.length) {
+            res.status(404).send('User not found');
+            return;
+        }
+        
+        const user = result.rows[0];
+        user['selfProfile'] = username === loggedInUser;
+
+            // get user's liked projects
+            db.query(`
+            SELECT 
+                projects.uuid, 
+                projects.name, 
+                projects.images,
+                (SELECT count(*) FROM comments WHERE comments.project_id = projects.uuid) AS comment_count,
+                (SELECT count(*) FROM likes WHERE likes.project_id = projects.uuid) AS likes_count,
+                (SELECT id FROM users WHERE users.id = projects.user_id) AS owner_id,
+                (SELECT username FROM users WHERE users.id = projects.user_id),
+                (SELECT gh_avatar FROM users WHERE users.id = projects.user_id),
+
+                (SELECT array_agg(tags.name::TEXT)
+                FROM projects_tags
+                INNER JOIN tags
+                ON tags.id = projects_tags.tag_id
+                WHERE projects_tags.project_id = projects.uuid) AS tags
+
+            FROM likes
+            INNER JOIN projects
+            ON likes.project_id = projects.uuid
+            WHERE likes.user_id = $1
+            LIMIT $2 OFFSET($3 - 1) * $2;
+        
+        `, [user.id, offset, page], (err: any, result: any) => {
+            if (err) console.log(err);
+
+            const likedProjects = result.rows;
+
+            db.query(
+            `            
+            SELECT COUNT(*)
+            FROM likes
+            INNER JOIN projects
+            ON likes.project_id = projects.uuid
+            WHERE likes.user_id = $1;           
+            `, [user.id], (err: any, result: any) => {
+                if (err) console.log(err);
+
+                const total: number = parseInt(result.rows[0].count);
+                const hasMore = Math.ceil(total / parseInt(offset)) === parseInt(page) ? false : true;
+                    
+                res.json({
+                    data: likedProjects,
+                    hasMore,
+                    total
+                });
+            });
+        });
+    });
+});
+
+
+export default router; 
--- src/routes/getUserProfile.ts
+++ src/routes/getUserProfile.ts
@@ -38,95 +38,9 @@ router.get('/:username', function (req, res, next) {
         const user = result.rows[0];
         user['selfProfile'] = username === loggedInUser;
 
-        // get user's saved projects
-        db.query(`
-        SELECT 
-            projects.uuid, 
-            projects.name, 
-            projects.images,
-            (SELECT count(*) FROM comments WHERE comments.project_id = projects.uuid) AS comment_count,
-            (SELECT count(*) FROM likes WHERE likes.project_id = projects.uuid) AS likes_count,
-            (SELECT id FROM users WHERE users.id = projects.user_id) AS owner_id,
-            (SELECT username FROM users WHERE users.id = projects.user_id),
-            (SELECT gh_avatar FROM users WHERE users.id = projects.user_id),
-        
-            (SELECT array_agg(tags.name::TEXT)
-            FROM projects_tags
-            INNER JOIN tags
-            ON tags.id = projects_tags.tag_id
-            WHERE projects_tags.project_id = projects.uuid) AS tags
-        
-        FROM user_saved_projects
-        INNER JOIN projects
-        ON projects.uuid = user_saved_projects.project_id
-        WHERE user_saved_projects.user_id = $1; 
-        
-        `, [user.id], (err: any, result: any) => {
-            if (err) console.log(err);
-
-            const savedProjects = result.rows;
-
-            // get user's liked projects
-            db.query(`
-            SELECT 
-                projects.uuid, 
-                projects.name, 
-                projects.images,
-                (SELECT count(*) FROM comments WHERE comments.project_id = projects.uuid) AS comment_count,
-                (SELECT count(*) FROM likes WHERE likes.project_id = projects.uuid) AS likes_count,
-                (SELECT id FROM users WHERE users.id = projects.user_id) AS owner_id,
-                (SELECT username FROM users WHERE users.id = projects.user_id),
-                (SELECT gh_avatar FROM users WHERE users.id = projects.user_id),
-
-                (SELECT array_agg(tags.name::TEXT)
-                FROM projects_tags
-                INNER JOIN tags
-                ON tags.id = projects_tags.tag_id
-                WHERE projects_tags.project_id = projects.uuid) AS tags
-
-            FROM likes
-            INNER JOIN projects
-            ON likes.project_id = projects.uuid
-            WHERE likes.user_id = $1;
-        
-        `, [user.id], (err: any, result: any) => {
-                if (err) console.log(err);
-
-                const likedProjects = result.rows;
-
-                // get user's projects
-                db.query(`
-                SELECT 
-                    projects.uuid, 
-                    projects.name,
-                    projects.images,
-                    (SELECT count(*) FROM comments WHERE comments.project_id = projects.uuid) AS comment_count,
-                    (SELECT count(*) FROM likes WHERE likes.project_id = projects.uuid) AS likes_count,
-                    (SELECT id FROM users WHERE users.id = projects.user_id) AS owner_id,
-                    (SELECT username FROM users WHERE users.id = projects.user_id),
-                    (SELECT gh_avatar FROM users WHERE users.id = projects.user_id),
-
-                    (SELECT array_agg(tags.name::TEXT)
-                    FROM projects_tags
-                    INNER JOIN tags
-                    ON tags.id = projects_tags.tag_id
-                    WHERE projects_tags.project_id = projects.uuid) AS tags
-
-                FROM projects
-                WHERE user_id = $1;
-        `, [user.id], (err: any, result: any) => {
-                    if (err) console.log(err);
-
-                    const userProjects = result.rows;
-
-                    res.send({
-                        data: {user, likedProjects, savedProjects, userProjects},
-                    })
-                })
-            })
-        })
+        res.json({ user });
     });
 });
 
 
-export default router;
+export default router; 
